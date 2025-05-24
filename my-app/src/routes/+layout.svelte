4aedb64c3553dd4376e4dc4925791c03418aebdc
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2025-05-24 15:48:36 -0700

    Initialized the front end of the project using Svelte

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	let { children } = $props();
+</script>
+<!-- 
+<head>
+    <title>CountryGUESSR</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Gregory Payton">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head> -->
+
+<nav>
+    <div>
+        <h1>CountryGUESSR</h1>
+    </div>
+</nav>
+
+ {@render children()}
+
+ <style>
+    h1 {
+        font-variant: small-caps;
+        color: darkgreen;
+    }
+ </style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+
+<p> Yo what's going on this is the home page for my new web application called CountryGUESSR</p>
