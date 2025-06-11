8850910ea54f358f68d5fc17c3167f53c4406299
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2025-06-11 11:46:28 -0700

    Added favicon.co

--- my-app/src/lib/index.ts
+++ my-app/src/lib/index.ts
@@ -1,7 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
-// import { createUser, getUserFromGoogleId, getCountryFacts } from "./server/crud";
+import { getFactsByID } from "./server/crud";
 
 console.log("Running index.ts...");
+
+const cca3="ERI";
+const res = await getFactsByID(cca3);
+console.log(res);
 
 console.log("Finished running index.ts...")
 
