3a60c40cdb01b391877d64017f6c988846ae6b4b
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2025-06-11 15:11:21 -0700

    updated api

--- my-app/src/routes/play/+page.svelte
+++ my-app/src/routes/play/+page.svelte
@@ -10,6 +10,7 @@
     import { getRandomInt } from '$lib/helper';
     import type { gameState } from '$lib/gameState';
     import { ConsoleLogWriter } from 'drizzle-orm';
+    import { KnuthShuffle } from '$lib/helper';
 
     let answer = $state("");
     let correct = $state(false);
@@ -109,7 +110,8 @@
     onMount(async() =>{
         console.log("onMount()...");
         if(countries.length === 0){
-            countries = await idbManager.getCountrys();
+            //Shuffle countries to ensure randomness
+            countries = KnuthShuffle(await idbManager.getCountrys());
             console.log("populated countries...");
         }
         //Get state
@@ -183,13 +185,13 @@
         }
     
     }
-
     async function getNewCountry(){
         //First reset facts pointer
         console.log("GAMEPLAY: RESETTING CURRFACTSPTR")
         currFactsPtr = 0;
         console.log("GAMEPLAY: GETTING NEW COUNTRIES")
         //Get a new country from countries left
+        console.log(countriesLeft.length);
         currCountry = countriesLeft[(getRandomInt(0, countriesLeft.length))];
         await getNewFacts();
     }
@@ -226,18 +228,17 @@
             loss = true;
         }
     }
-
 </script>
 
 <section id="gameplay" class="flex justify-center pt-3 flex-col md:flex-row md:justify-evenly">
     <div class="flex md:min-w-[40vw] md:max-w-[50vw]">
         <section id="main-gampeplay">
             <section id="canvas-section"class="flex justify-center">
-                <canvas id="canvas" width="300" height="150" bind:this={canvas}>This canvas shows the game state</canvas>   
+                <canvas id="canvas" width="300" height="150" bind:this={canvas} aria-label="Canvas showing the country flag">{currCountry?.flags.alt}</canvas>   
             </section>
             
             {#if countriesLeft.length > 0}
-            <section id="fact-section" class="bg-slate-200 m-2 p-4 md:max-w-[50vw] md:min-w[50vw]">
+            <section id="fact-section" class="bg-slate-200 m-2 p-4 md:max-w-[50vw] md:min-w[50vw]" aria-label="A fact about the current country is displayed">
                 <div class="md:w-[500px] break-words whitespace-normal">
                     <p>{@html currFactsPtr < currFacts.length ? currFacts[currFactsPtr].fact : `You did not guess it! The country was: ${currCountry?.name.common}`}</p>
                 </div>
@@ -249,15 +250,15 @@
             </section>
             {/if}
 
-            <section id="options" class="flex justify-center p-2">
+            <section id="options" class="flex justify-center p-2" aria-label="Reset Button">
                 <button class="rounded-md bg-red-400 p-2 cursor-pointer" onclick={resetState}>Reset</button>
             </section>
 
-            <section id="query-section md:max-w-[40vw]">
+            <section id="query-section md:max-w-[40vw]" aria-label="Search Bar for countries">
                 <Search bind:searchTerm on:input={searchCountries} on:focus={handleFocus} on:blur={handleBlur}></Search>
             </section>
 
-            <section id="answer-section">
+            <section id="answer-section" aria-label="Answer Feedback here">
                 {#if showAnswer}
                 <div>
                     <p id="answer" class="text-center font-bold {correct ? "text-green-500" : "text-red-500"}" out:fade>{answer} {correct ? "(correct)" : "(incorrect)"}</p>
@@ -267,7 +268,7 @@
             
             <!-- Only show dropdown if there are still facts left -->
             {#if inputSelected }
-            <section id="results-section" class="max-h-[30vh] md:max-w-[35vw] overflow-auto m-1 break-words">
+            <section id="results-section" class="max-h-[30vh] md:max-w-[35vw] overflow-auto m-1 break-words" aria-label="Dropdown with countries">
                 {#each filteredCountries as res}
                     <button class="hover:bg-amber-100 hover:cursor-pointer rounded-md bg-slate-100 inline p-1 m-1" onmousedown={() => handleResultMouseDown(res)}>{res.name.common}</button>
                 {/each}
@@ -277,7 +278,7 @@
     </div>
        
       
-    <section class="table-section">
+    <section class="table-section" aria-label="List of countries correclty guessed">
         <table class="table-auto m-4">
             <thead>
                 <tr>
